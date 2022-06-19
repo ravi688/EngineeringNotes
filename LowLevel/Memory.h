35824c14b121ac7727d03d7864c4431715069dca
ravi688 <rp0412204@gmail.com>
2022-06-19 15:12:02 +0530

    Added Memory functions

---
+++ LowLevel/Memory.h
@@ -0,0 +1,12 @@
+
+
+ memchr() // Returns a pointer to the first byte in a block 
+	  // matching the specified character
+ memcmp() // Compares two blocks of memory, returning any
+	  // differences
+ memcpy() // Copies two nonoverlapping blocks of memory
+ memmove() // Copies two blocks of memory, which may or may not
+	   // overlap
+ memset() // Fills a block of memory with the specified byte
+
+

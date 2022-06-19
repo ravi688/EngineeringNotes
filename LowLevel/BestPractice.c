4005150c41fb07859f7f64efcc3628ce1b729243
ravi688 <rp0412204@gmail.com>
2022-06-19 12:56:09 +0530

    Added Best Practice

---
+++ LowLevel/BestPractice.c
@@ -0,0 +1,36 @@
+
+ A typical source file header block.
+
+
+ /*
+  * **********************************************************
+  *
+  * PROJECT: The project's name goes here.
+  *
+  * TITLE: The FILE'S title (not the project title).
+  *
+  * FUNCTION: What the functions(s) in this file does. More than
+  * 	one line if necessary.
+  *
+  * INPUTS: What generally is passed to the functions.
+  *
+  * OUTPUTS: What the functions return.
+  *
+  * RETURNS: Some functions don't return normally; sa so if necessary
+  *
+  * WRITTEN: When the file was created
+  *
+  * CALLS: Significant calls to other parts of program.
+  *
+  *
+  * CALLED BY: Who (generally) calls these functions.
+  *
+  * AUTHOR: You name.
+  *
+  * NOTES: Modifications, special considerations, and so on.
+  *
+  * COPYRIGHT 1992: By whomever. All rights reserved. All wrongs
+  * deserved.
+  *
+  * /
+
